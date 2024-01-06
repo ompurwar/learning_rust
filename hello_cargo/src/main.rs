e96517176bef1a10b4950d5cacf11c12d5be0330
Om Purwar <kaidranzerom@gmail.com>
2024-01-06 18:28:21 +0530

    :sparkles: learing array

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("Hello, world2!");
     let a = ("sdhgsajd", 12);
-    println!("{}-{}", a.0, a.1)
+    let array = [1, 2, 3];
+    println!("{}-{}-{}", a.0, a.1, array[1])
 }
