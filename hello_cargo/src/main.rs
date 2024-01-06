df87a55cd9fc89f63a64d247a12fc1b2a0877a20
Om Purwar <kaidranzerom@gmail.com>
2024-01-06 18:30:10 +0530

    :sparkles: Looping through an array using for in

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -2,5 +2,8 @@ fn main() {
     println!("Hello, world2!");
     let a = ("sdhgsajd", 12);
     let array = [1, 2, 3];
-    println!("{}-{}-{}", a.0, a.1, array[1])
+    println!("{}-{}-{}", a.0, a.1, array[1]);
+    for item in array.iter() {
+        print!("printing {}\n", item)
+    }
 }
